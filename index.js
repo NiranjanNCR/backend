574eee45287400d271639b2d5bf365e05f395b7e
Niranjan <nrjsingh5123@gmail.com>
2024-12-01 15:53:19 +0530

    env

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: 'https://pehlakadamm.netlify.app/', // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    
+
   })
 );
 app.use(bodyParser.json());
-app.use('/uploads', express.dynamic(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.dynamic(path.join(__dirname, 'uploads')));
 
 
 // MongoDB setup
@@ -26,6 +27,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+
 
 
 const db = mongoose.connection;
