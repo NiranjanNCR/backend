24b52405269d7584988a92c31610f66b127d3c14
NiranjanNCR <111573715+NiranjanNCR@users.noreply.github.com>
2024-12-02 09:19:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.post('/submit', async (req, res) => {
 });
 
 // Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
